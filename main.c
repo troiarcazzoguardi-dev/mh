d9b883e24dcda9d356fda439237eb2a0aa3ac0de
troiarcazzoguardi-dev <troiarcazzoguardi@gmail.com>
2025-12-27 09:18:55 +0100

    Update main.c

--- main.c
+++ main.c
@@ -17,14 +17,15 @@
 
 /* ================= CONFIG ================= */
 #define MAX_PKT_SIZE 1500
-#define DEFAULT_RATE_PPS 20000
+#define DEFAULT_PAYLOAD 64
+#define DEFAULT_TIME 10
 
 /* ================= OPTIONS ================= */
 struct opts {
     int port;
-    int duration;
-    int rate;
-    bool af_xdp;
+    int time_sec;          // -t
+    int payload_size;      // -d
+    bool af_xdp;           // -X
     char iface[IFNAMSIZ];
     char target[64];
 };
@@ -39,8 +40,12 @@ struct packet {
 static uint16_t checksum(uint16_t *buf, size_t len)
 {
     uint32_t sum = 0;
-    while (len > 1) { sum += *buf++; len -= 2; }
-    if (len) sum += *(uint8_t *)buf;
+    while (len > 1) {
+        sum += *buf++;
+        len -= 2;
+    }
+    if (len)
+        sum += *(uint8_t *)buf;
 
     sum = (sum >> 16) + (sum & 0xffff);
     sum += (sum >> 16);
@@ -51,14 +56,18 @@ static uint16_t checksum(uint16_t *buf, size_t len)
 static int autodetect_iface(const char *dst_ip, char *iface, size_t len)
 {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) return -1;
+    if (sock < 0)
+        return -1;
 
     struct sockaddr_in dst = {0};
     dst.sin_family = AF_INET;
     dst.sin_port = htons(53);
     inet_pton(AF_INET, dst_ip, &dst.sin_addr);
 
-    if (connect(sock, (struct sockaddr *)&dst, sizeof(dst)) < 0) { close(sock); return -1; }
+    if (connect(sock, (struct sockaddr *)&dst, sizeof(dst)) < 0) {
+        close(sock);
+        return -1;
+    }
 
     struct sockaddr_in src = {0};
     socklen_t slen = sizeof(src);
@@ -68,7 +77,8 @@ static int autodetect_iface(const char *dst_ip, char *iface, size_t len)
     getifaddrs(&ifaddr);
 
     for (ifa = ifaddr; ifa; ifa = ifa->ifa_next) {
-        if (!ifa->ifa_addr) continue;
+        if (!ifa->ifa_addr)
+            continue;
         if (ifa->ifa_addr->sa_family == AF_INET) {
             struct sockaddr_in *in = (struct sockaddr_in *)ifa->ifa_addr;
             if (in->sin_addr.s_addr == src.sin_addr.s_addr) {
@@ -94,13 +104,11 @@ static void build_udp(struct packet *p, struct opts *o)
     struct udphdr *udp = (struct udphdr *)(p->buf + sizeof(*ip));
     uint8_t *payload = p->buf + sizeof(*ip) + sizeof(*udp);
 
-    const char *msg = "TX_TEST";
-    size_t plen = strlen(msg);
-    memcpy(payload, msg, plen);
+    memset(payload, 'A', o->payload_size);
 
     udp->source = htons(12345);
     udp->dest   = htons(o->port);
-    udp->len    = htons(sizeof(*udp) + plen);
+    udp->len    = htons(sizeof(*udp) + o->payload_size);
 
     ip->ihl      = 5;
     ip->version  = 4;
@@ -108,33 +116,42 @@ static void build_udp(struct packet *p, struct opts *o)
     ip->protocol = IPPROTO_UDP;
     inet_pton(AF_INET, o->target, &ip->daddr);
 
-    ip->tot_len = htons(sizeof(*ip) + sizeof(*udp) + plen);
+    ip->tot_len = htons(sizeof(*ip) + sizeof(*udp) + o->payload_size);
     ip->check   = checksum((uint16_t *)ip, sizeof(*ip));
 
-    p->len = sizeof(*ip) + sizeof(*udp) + plen;
+    p->len = sizeof(*ip) + sizeof(*udp) + o->payload_size;
 }
 
 /* ================= CLI ================= */
 static void parse_cli(int argc, char **argv, struct opts *o)
 {
     memset(o, 0, sizeof(*o));
-    o->rate = DEFAULT_RATE_PPS;
+    o->payload_size = DEFAULT_PAYLOAD;
+    o->time_sec = DEFAULT_TIME;
 
     int c;
-    while ((c = getopt(argc, argv, "p:d:r:i:t:X")) != -1) {
+    while ((c = getopt(argc, argv, "p:d:t:i:X")) != -1) {
         switch (c) {
         case 'p': o->port = atoi(optarg); break;
-        case 'd': o->duration = atoi(optarg); break;
-        case 'r': o->rate = atoi(optarg); break;
+        case 'd': o->payload_size = atoi(optarg); break;
+        case 't': o->time_sec = atoi(optarg); break;
         case 'i': strncpy(o->iface, optarg, IFNAMSIZ - 1); break;
-        case 't': strncpy(o->target, optarg, sizeof(o->target) - 1); break;
         case 'X': o->af_xdp = true; break;
-        default: exit(1);
+        default:
+            fprintf(stderr, "Usage: -p port -d payload -t time -i iface [-X] target\n");
+            exit(1);
         }
     }
 
-    if (!o->port || !o->duration || !o->target[0]) {
-        fprintf(stderr, "Usage: -p port -d sec -t target [-i iface] [-X]\n");
+    if (optind >= argc || !o->port) {
+        fprintf(stderr, "Missing target or port\n");
+        exit(1);
+    }
+
+    strncpy(o->target, argv[optind], sizeof(o->target) - 1);
+
+    if (o->payload_size <= 0 || o->payload_size > 1400) {
+        fprintf(stderr, "Payload must be 1–1400 bytes\n");
         exit(1);
     }
 }
@@ -143,7 +160,10 @@ static void parse_cli(int argc, char **argv, struct opts *o)
 static void udp_tx(struct packet *p, struct opts *o)
 {
     int s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
-    if (s < 0) { perror("socket"); return; }
+    if (s < 0) {
+        perror("socket");
+        return;
+    }
 
     int one = 1;
     setsockopt(s, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one));
@@ -155,8 +175,9 @@ static void udp_tx(struct packet *p, struct opts *o)
     time_t start = time(NULL);
     uint64_t sent = 0;
 
-    while (time(NULL) - start < o->duration) {
-        sendto(s, p->buf, p->len, 0, (struct sockaddr *)&dst, sizeof(dst));
+    while (time(NULL) - start < o->time_sec) {
+        sendto(s, p->buf, p->len, 0,
+               (struct sockaddr *)&dst, sizeof(dst));
         sent++;
     }
 
@@ -181,13 +202,12 @@ int main(int argc, char **argv)
         }
     }
 
-    build_udp(&pkt, &o);
-
     if (o.af_xdp) {
-        printf("[!] AF_XDP richiesto, ma su questa macchina potrebbe non essere supportato\n");
-        printf("[!] Fallback automatico a UDP\n");
+        printf("[!] your machine does not support this flag\n");
+        printf("[!] fallback to UDP\n");
     }
 
+    build_udp(&pkt, &o);
     udp_tx(&pkt, &o);
     return 0;
 }
